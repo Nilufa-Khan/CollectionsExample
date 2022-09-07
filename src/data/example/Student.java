8b3454516e8c6a04b57f8731d3940c47bb36b6b4
Nilufa Khan <khannilufa5@gmail.com>
2022-09-07 22:55:09 +0530

    Dclaring scanner class for taking input from user

--- src/data/example/Student.java
+++ src/data/example/Student.java
@@ -18,6 +18,7 @@ public class Student {
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array");
         int size = sc.nextInt();
