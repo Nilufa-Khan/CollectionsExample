7a14231f7c78499622eea81dc8d6fc9a33073fc2
Nilufa Khan <khannilufa5@gmail.com>
2022-09-07 22:16:58 +0530

    Taking input from scanner

--- src/data/example/Rectangle.java
+++ src/data/example/Rectangle.java
@@ -18,13 +18,24 @@ public class Rectangle {
 
     public static void main(String[] args) {
         ////Declaring array of objects
-        Rectangle[] obj = new Rectangle[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array ");
+        int size = sc.nextInt();
+        Rectangle[] obj = new Rectangle[size];
         //Initializing array
-        obj[0] = new Rectangle(2,4);
+       /* obj[0] = new Rectangle(2,4);
         obj[1] = new Rectangle(3,4);
         obj[2] = new Rectangle(4,4);
         obj[3] = new Rectangle(5,4);
-        obj[4] = new Rectangle(6,4);
+        obj[4] = new Rectangle(6,4);*/
+
+        for (int i = 0; i < obj.length; i++) {
+            System.out.println(i + " index Enter length");
+            int len = sc.nextInt();
+            System.out.println(i + " index Enter bredth");
+            int bredth = sc.nextInt();
+            obj[i] = new Rectangle(len,bredth);
+        }
 
         // Traversing through the for loop and retrieving area value
         for(int i = 0; i < obj.length; i++){
