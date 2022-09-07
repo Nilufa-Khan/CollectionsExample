0551c948d20393f0ccfad46571c0ccc19ffbcc8e
Nilufa Khan <khannilufa5@gmail.com>
2022-09-07 22:03:13 +0530

    Created parameterised constructor

--- src/data/example/Rectangle.java
+++ src/data/example/Rectangle.java
@@ -6,6 +6,7 @@ public class Rectangle {
 
     private int length;
     private int breadth;
+
     public Rectangle(int length,int breadth){
         this.length = length;
         this.breadth = breadth;
